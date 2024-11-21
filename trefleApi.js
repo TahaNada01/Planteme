2c975033234977d60ca09fb97a62a7e7c58a52fa
TahaNada01 <117838958+TahaNada01@users.noreply.github.com>
2024-11-21 14:13:36 +0100

    Update trefleApi.js

--- trefleApi.js
+++ trefleApi.js
@@ -1,21 +1,34 @@
 const axios = require('axios');
 
-const apiKey = 'BJuTakuXkBL51DzqvMTMw3Ief8lJl14l2Ye78C0L74o'; // Remplacez par votre clé API
+const apiKey = 'HWXzNbxYVPAJw3sCwX4uEIRRfgBrVYkuTIE4NHgfwyhbawAvOY'; // Remplacez par votre clé API Plant.id
 
-const trefleApi = axios.create({
-  baseURL: 'https://trefle.io/api/v1',
+const plantIdApi = axios.create({
+  baseURL: 'https://plant.id/api/v3',
   headers: {
-    Authorization: `Bearer ${apiKey}`,
+    'Api-Key': apiKey,
   },
 });
 
-const getPlantNameByImage = async (imageUri) => {
+const getPlantNameByImage = async (imageBuffer) => {
   try {
-    const response = await trefleApi.post('/identify', { image: imageUri });
-    if (response.data.data && response.data.data.length > 0) {
-      return response.data.data[0].common_name;
+    // Préparation des données à envoyer
+    const formData = new FormData();
+    formData.append('images', imageBuffer, 'image.jpg'); // Ajoute l'image au formulaire
+
+    // Optionnel : Ajoutez des données supplémentaires si nécessaire
+    formData.append('organs', 'leaf'); // Exemple : indique que c'est une feuille
+    formData.append('include-related-images', 'true');
+
+    // Requête POST vers l'API
+    const response = await plantIdApi.post('/identify', formData, {
+      headers: formData.getHeaders(), // Ajoute les en-têtes nécessaires pour le FormData
+    });
+
+    // Traitement de la réponse
+    if (response.data.suggestions && response.data.suggestions.length > 0) {
+      return response.data.suggestions[0].plant_name; // Retourne le nom de la plante
     }
-    return null;
+    return null; // Aucune plante trouvée
   } catch (error) {
     console.error('Erreur lors de la récupération des données de la plante:', error);
     return null;
